fdd47ab6f39a14fa80adba5a077a6030b51b85e4
악덕 <rudejrqkr97@gmail.com>
2024-11-03 23:11:46 +0900

    feat: 게시판 저장 기능 세팅
    - 'BoardController.java'에 게시판 저장을 위한 'save' 메서드 추가
    - 'save.html' 파일 생성하여 게시판 저장 폼 구현
    - 사용자가 게시판에 글을 작성하고 저장할 수 있는 기능을 제공

--- src/main/java/com/virtue/mybatisnotice/controller/BoardController.java
+++ src/main/java/com/virtue/mybatisnotice/controller/BoardController.java
@@ -1,11 +1,20 @@
 package com.virtue.mybatisnotice.controller;
 
+import com.virtue.mybatisnotice.dto.BoardDTO;
 import com.virtue.mybatisnotice.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor // final이 붙은 필드를 자동으로 생성하는 생성자를 만들어줌
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
 }
