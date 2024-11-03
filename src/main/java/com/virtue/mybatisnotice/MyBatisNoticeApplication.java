43acab1a85f4c9e695eac0a49f354df8e9b81a64
악덕 <rudejrqkr97@gmail.com>
2024-11-03 21:32:20 +0900

    init

---
+++ src/main/java/com/virtue/mybatisnotice/MyBatisNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.virtue.mybatisnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisNoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisNoticeApplication.class, args);
+    }
+
+}
